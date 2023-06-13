17e78d9d9267d78cd5adb62c6dc674dc5ccff365
Raul Bache <raul@bache.se>
2023-06-13 10:14:06 +0200

    Granularity

--- arnehttp/misc.go
+++ arnehttp/misc.go
@@ -1,13 +1,17 @@
 package arnehttp
 
 import (
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"net/http"
+	"strings"
 	"time"
 )
 
 type wrappedW struct {
 	http.ResponseWriter
+	r       *http.Request
+	start   time.Time
 	written int
 	code    int
 }
@@ -23,16 +27,49 @@ func (w *wrappedW) WriteHeader(statusCode int) {
 	w.ResponseWriter.WriteHeader(statusCode)
 }
 
-func LoggingHandler(h http.Handler) http.HandlerFunc {
+type LoggingOpt func(w *wrappedW, e *zerolog.Event) *zerolog.Event
+
+func WithDuration() LoggingOpt {
+	return func(w *wrappedW, e *zerolog.Event) *zerolog.Event {
+		return e.Dur("duration", time.Now().Sub(w.start))
+	}
+}
+
+func WithPath() LoggingOpt {
+	return func(w *wrappedW, e *zerolog.Event) *zerolog.Event {
+		return e.Str("path", w.r.URL.Path)
+	}
+}
+
+func WithCode() LoggingOpt {
+	return func(w *wrappedW, e *zerolog.Event) *zerolog.Event {
+		return e.Int("code", w.code)
+	}
+}
+
+func WithLength() LoggingOpt {
+	return func(w *wrappedW, e *zerolog.Event) *zerolog.Event {
+		return e.Int("len", w.written)
+	}
+}
+
+func IgnorePath(path string) LoggingOpt {
+	return func(w *wrappedW, e *zerolog.Event) *zerolog.Event {
+		if strings.HasPrefix(w.r.URL.Path, path) {
+			return e.Discard()
+		}
+		return e
+	}
+}
+
+func LoggingHandler(h http.Handler, opts ...LoggingOpt) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		before := time.Now()
-		wr := wrappedW{ResponseWriter: w, code: 200}
+		wr := wrappedW{ResponseWriter: w, code: 200, start: time.Now(), r: r}
 		h.ServeHTTP(&wr, r)
-		log.Debug().
-			Str("path", r.URL.Path).
-			Dur("duration", time.Now().Sub(before)).
-			Int("code", wr.code).
-			Int("len", wr.written).
-			Msg("access")
+		evt := log.Debug()
+		for _, opt := range opts {
+			evt = opt(&wr, evt)
+		}
+		evt.Msg("access")
 	}
 }
